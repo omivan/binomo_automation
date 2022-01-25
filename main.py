13259f02cdde534f9e5004ef3697f6dac0c5e3f2
omivan <ivan.omelchenk@gmail.com>
2022-01-25 17:32:15 +0200

    main.py changed(time of execution was added)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from economical_calendar_parser import get_quotes
-
+import time
 
 
 if __name__ == '__main__':
+    time_before = time.time()
     while True:
-        previous_quote, current_quote = get_quotes("Индекс настроений в деловых кругах", "Business Climate")
+        previous_quote, current_quote = get_quotes("Индикатор уверенности потребителей", "Consumer confidence")
         print(previous_quote, current_quote)
         if current_quote != False:
+            time_after = time.time()
+            print(f"Time: {round(time_after - time_before, 2)} ")
             break
